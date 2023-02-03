ad7442f461255a2cf17e6495654aaeec5b178f6e
Manoj Neupane <manpane98@gmail.com>
2023-02-03 21:04:25 +0545

    added particle animation for background

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 nav = document.querySelector("nav")
 hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click",e=>{
@@ -12,3 +11,26 @@ hamburger.addEventListener("click",e=>{
         nav.classList.remove("nav-shown");
     }
 })
+page = document.getElementById("page")
+
+function randomizeParticle(particle){
+    particle.style.bottom = "-5px"
+    let randomSecs = Math.floor(Math.random()*20);
+    let random_size = Math.floor(Math.random()*20);
+    let random_x = Math.floor(Math.random()*window.innerWidth);
+    let random_y = Math.floor(Math.random()*600);
+    if(randomSecs<5){randomSecs = 5;}
+    particle.style.position = "fixed"
+    particle.style.animation = `particle-animation ${randomSecs}s 100`
+    particle.style.width = `${random_size}px`;
+    particle.style.height = `${random_size}px`;
+    particle.style.left = `${random_x}px`;
+    particle.style.top = `-${random_y}px`;
+    return particle
+}
+total_particles = 50
+for(let i = 0;i<total_particles;i++){
+    let new_particle = document.createElement("div")
+    new_particle.setAttribute("class","particle")
+    document.body.appendChild(randomizeParticle(new_particle));
+}
