18ecd590c4c454e4edb4767ff2abe75b7c46388d
Manoj Neupane <manpane98@gmail.com>
2023-02-10 22:53:10 +0545

    dynamic title and documented some code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-page = document.getElementById("page")
-nav = document.querySelector("nav")
-hamburger = document.querySelector(".hamburger");
+page = document.getElementById("page") // the main content element
+nav = document.querySelector("nav") // the whole nav element
+hamburger = document.querySelector(".hamburger"); // hamburger icon (for mobile)
 
-function closeNav(){
+function closeNav(){ // closes the side navigation (for mobile)
     nav.classList.add("nav-hidden");
     nav.classList.remove("nav-shown");
 }
-function openNav(){
+function openNav(){ // opens the side navigation (for mobile)
     nav.classList.remove("nav-hidden");
     nav.classList.add("nav-shown");
 }
-function toggleHamburger(){
+function toggleHamburger(){ // animating the hamburger
     hamburger.classList.toggle("hamclosed");
 }
-function toggleNav(){
+function toggleNav(){ // function to handle navigation open and close
     toggleHamburger()
     if (nav.classList.contains("nav-hidden")){
         openNav()
@@ -23,8 +23,8 @@ function toggleNav(){
     }
 }
 
-hamburger.addEventListener("click",e=>{
-    toggleNav()
+hamburger.addEventListener("click",e=>{//when hamburger menu is clicked
+    toggleNav() 
 })
 page.addEventListener("click",e=>{
     if (nav.classList.contains("nav-shown")){
@@ -32,8 +32,9 @@ page.addEventListener("click",e=>{
         toggleHamburger();
     }
 })
-
+//Background Particle dropping animation
 function randomizeParticle(particle){
+    // randomizes the values of the given particle element according to the screen resolution
     particle.style.bottom = "-5px"
     let randomSecs = Math.floor(Math.random()*20);
     let random_size = Math.floor(Math.random()*20);
@@ -48,28 +49,34 @@ function randomizeParticle(particle){
     particle.style.top = `-${random_y}px`;
     return particle
 }
-total_particles = 50
+
+total_particles = 50 // total particles elements for animation
 for(let i = 0;i<total_particles;i++){
+    //creating new html div elements with particle class and adding to the document by randomizing the values
     let new_particle = document.createElement("div")
     new_particle.setAttribute("class","particle")
     document.body.appendChild(randomizeParticle(new_particle));
 }
 
 
-window.addEventListener("resize",e=>{
+window.addEventListener("resize",e=>{ // updating the particles position with window resize
     particles = document.getElementsByClassName("particle");
     for(let particle of particles){
         randomizeParticle(particle);
     }
 })
 
+// Transparency of current page title at the top when document is scrolled.
 window.addEventListener("scroll", (event) => {
     let content_title = document.querySelector("h1.content-title");
-    if (this.scrollY!=0){
+    if (this.scrollY!=0){ // when the user is a the top (not scrolled)
         content_title.style.backgroundColor = "var(--content-title-bg-opaque)"
-    }else{
+    }else{ // when the user scrolls the page
         content_title.style.backgroundColor = "var(--content-title-bg)"
     }
 });
 
 
+// Setting the title of the window according to the current page
+curr_page = document.querySelector(".current-page")
+document.title = curr_page.textContent + " - Manoj Neupane"
